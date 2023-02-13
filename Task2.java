95f558e063a218768bcbd63a023b7af8bf3c591f
VladislavSt1 <70508810+VladislavSt1@users.noreply.github.com>
2023-02-13 20:52:42 +0300

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+         int a = 21;
+         int b = 8;
+        System.out.println(a + " / " + b + " = " + (a / b) + " и " + (a % b) + " в остатке");
+
+    }
+}
